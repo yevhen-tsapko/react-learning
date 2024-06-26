a53d4360bddd8ee16f13675530656b0180e03858
Yevhen Tsapko <evgentsa@gmail.com>
2024-06-26 15:34:53 +0300

    make ccs-class with cls-library

--- src/Message.js
+++ src/Message.js
@@ -1,11 +1,16 @@
-import { Children } from "react";
+import clsx from "clsx";
 import "./Message.css";
 
 const Message = ({ variant, outlined, elevated, children }) => {
-  const classNames = ["alert", variant];
-  if (outlined) classNames.push("is-outlined");
-  if (elevated) classNames.push("is-elevated");
-  console.log("classNames", classNames);
-  return <p className={classNames.join(" ")}>{children}</p>;
+  return (
+    <p
+      className={clsx("alert", variant, {
+        "is-outlined": outlined,
+        "is-elevated": elevated,
+      })}
+    >
+      {children}
+    </p>
+  );
 };
 export default Message;
