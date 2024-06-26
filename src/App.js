310477e763a51cc777c51dad68a06aa53b3f0d96
Yevhen Tsapko <evgentsa@gmail.com>
2024-06-26 12:08:28 +0300

    vanila css, class composition

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import "./App.css";
 import Message from "./Message";
-import BookList from "./component-1";
-const favouriteBooks = [
-  { id: "id-1", name: "JS for beginners" },
-  { id: "id-2", name: "React basics" },
-  { id: "id-3", name: "React Router overview" },
-  { id: "id-4", name: "Redux in depth" },
-];
+
 function App() {
   return (
     <>
-      {2 && <BookList books={favouriteBooks} />}
-      {3 > 2 ? <BookList books={favouriteBooks} /> : <p>Any book</p>}
       {" " && (
         <Message
           variant={"info"}
+          outlined
           children={"Would you like to browse our recommended products?"}
         />
       )}
       {
         <Message
           variant={"success"}
+          elevated
           children={"Would you like to browse our recommended products?"}
         />
       }
--- src/Message.js
+++ src/Message.js
@@ -1,26 +1,11 @@
 import { Children } from "react";
-const alertStyles = {
-  margin: 8,
-  padding: "12px 16px",
-  borderRadius: 4,
-  backgroundColor: "gray",
-  color: "grey",
-};
-const getColor = (variant) => {
-  switch (variant) {
-    case "info":
-      return "blue";
-    case "success":
-      return "green";
-    case "error":
-      return "red";
-    case "warning":
-      return "orange";
-    default:
-      throw new Error(`Unsupported variant prop value - ${variant}`);
-  }
-};
-const Message = ({ variant, children }) => {
-  return <p style={{ ...alertStyles, color: getColor(variant) }}>{children}</p>;
+import "./Message.css";
+
+const Message = ({ variant, outlined, elevated, children }) => {
+  const classNames = ["alert", variant];
+  if (outlined) classNames.push("is-outlined");
+  if (elevated) classNames.push("is-elevated");
+  console.log("classNames", classNames);
+  return <p className={classNames.join(" ")}>{children}</p>;
 };
 export default Message;
