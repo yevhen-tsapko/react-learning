587825283493fdff566a14ee277cf849178c465a
Yevhen Tsapko <evgentsa@gmail.com>
2024-06-27 16:59:43 +0300

    +form with Formik

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Basic from "./BasicForm";
 import SignUpForm from "./InputForm";
 
 function App() {
   return (
     <div>
       <SignUpForm onSubmit={(values) => console.log(values)} />
+      <Basic />
     </div>
   );
 }
