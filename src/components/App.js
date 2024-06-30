b952fa225d173cb96a0600a1a1cd04fd9cc0e6f7
Yevhen Tsapko <evgentsa@gmail.com>
2024-06-30 18:03:41 +0300

    route with dinamic param

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import { Route, Routes } from "react-router-dom";
+import { About } from "../pages/About";
+import { Home } from "../pages/Home";
+import { ProductDetails } from "../pages/ProductDetails";
+import { Products } from "../pages/Products";
+import { Container, Header, Logo, Link } from "./App.styled";
+
+export const App = () => {
+  return (
+    <Container>
+      <Header>
+        <Logo>
+          <span role="img" aria-label="computer icon">
+            💻
+          </span>{" "}
+          GoMerch Store
+        </Logo>
+        <nav>
+          <Link to="/" end>
+            Home
+          </Link>
+          <Link to="/about">About</Link>
+          <Link to="/products">Products</Link>
+        </nav>
+      </Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetails />} />
+      </Routes>
+    </Container>
+  );
+};
