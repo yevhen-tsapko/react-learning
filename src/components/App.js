a232bd1fb7817686f64b7499237d21c7332a0e1b
Yevhen Tsapko <evgentsa@gmail.com>
2024-07-01 14:44:03 +0300

    + index path

--- src/components/App.js
+++ src/components/App.js
@@ -3,39 +3,24 @@ import { About } from "../pages/About";
 import { Home } from "../pages/Home";
 import { ProductDetails } from "../pages/ProductDetails";
 import { Products } from "../pages/Products";
-import { Container, Header, Logo, Link } from "./App.styled";
 import { Mission } from "./Mission";
 import { Team } from "./Team";
 import { Reviews } from "./Reviews";
+import { SharedLayout } from "./SharedLayout";
 
 export const App = () => {
   return (
-    <Container>
-      <Header>
-        <Logo>
-          <span role="img" aria-label="computer icon">
-            💻
-          </span>{" "}
-          GoMerch Store
-        </Logo>
-        <nav>
-          <Link to="/" end>
-            Home
-          </Link>
-          <Link to="/about">About</Link>
-          <Link to="/products">Products</Link>
-        </nav>
-      </Header>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />}>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />}>
           <Route path="mission" element={<Mission />} />
           <Route path="team" element={<Team />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:id" element={<ProductDetails />} />
-      </Routes>
-    </Container>
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<ProductDetails />} />
+      </Route>
+    </Routes>
   );
 };
