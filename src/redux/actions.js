b17307e16dc25930b9df9f33507c7055e518dee1
Yevhen Tsapko <evgentsa@gmail.com>
2024-07-07 11:20:57 +0300

    redux with slice

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,9 +1,9 @@
-import { createAction } from "@reduxjs/toolkit";
-import { nanoid } from "nanoid";
+// import { createAction } from "@reduxjs/toolkit";
+// import { nanoid } from "nanoid";
 
-export const addTask = createAction("task/addTask", (text) => ({
-  payload: { text, id: nanoid(), completed: false },
-}));
-export const toggleCompleted = createAction("toggleCompleted");
-export const removeTask = createAction("task/toggleCompleted");
-export const setStatusFilter = createAction("setStatusFilter");
+// export const addTask = createAction("task/addTask", (text) => ({
+//   payload: { text, id: nanoid(), completed: false },
+// }));
+// export const toggleCompleted = createAction("toggleCompleted");
+// export const removeTask = createAction("task/toggleCompleted");
+// export const setStatusFilter = createAction("setStatusFilter");
